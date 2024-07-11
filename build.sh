0c0b1980de298e7f14b9606c40b701263dceb829
MarvinKatapult <ibingmarvin@gmail.com>
2024-07-11 23:07:48 +0200

    Updated Buildscript

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-COMP_CMD="gcc -o example example.c cvecs.c -Wall -Wextra -pedantic -g"
+BIN_NAME="example"
+CFLAGS="-Wall -Wextra -pedantic -g"
+COMP_CMD="gcc -o ${BIN_NAME} example.c cvecs.c ${CFLAGS}"
 
 execute() {
     echo $1
     eval $1
 }
 
-execute "$COMP_CMD"
+if [ "$1" = "clean" ]; then
+    execute "rm ${BIN_NAME}"
+else
+    execute "$COMP_CMD"
+fi
